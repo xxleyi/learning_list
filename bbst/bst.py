952e0c5fe5648551831c4ff756e7769d3c091a00
simon <1107432420@qq.com>
2019-07-22 09:20:24 +0800

    feat: add basic BST ADT

--- bbst/bst.py
+++ bbst/bst.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from bbst.bt import (
-    RB_RED,
-    RB_BLAC,
-    stature,
-    is_root,
-    is_lchi,
-    is_rchi,
-    has_par,
-    has_lch,
-    has_rch,
-    has_chi,
-    has_bot,
-    is_leaf,
-    sibling,
-    uncle,
-    visit,
-    Stack,
-    BinNode,
-)
+from bbst.bt import Entry, BinNode, BinTree
 
 
-class BST(object):
+class BST(BinTree):
     def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        # 命中节点的父亲
+        self._hot = None
+
+    @staticmethod
+    def _connect34(b1, b2, b3, bb1, bb2, bb3, bb4):
+        pass
+
+    @staticmethod
+    def _rotate_at(x):
+        pass
+
+    def _search_in(self, v, e):
+        if not v or e == v.data:
+            return v
+        self._hot = v
+        return self._search_in(e.lc if e < v.data else e.rc, e)
+
+    def search(self, e):
+        self._hot = None
+        return self._search_in(self._root, e)
+
+    def insert(self, e):
+        raise NotImplementedError("You should implement this method in subclass.")
+
+    def remove(self, e):
+        raise NotImplementedError("You should implement this method in subclass.")
 
 
 if __name__ == "__main__":

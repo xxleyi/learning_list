afe6164a783441888dcc9abb24b318e8a5176a2b
simon <1107432420@qq.com>
2019-08-30 09:37:18 +0800

    生产者-消费者模型

---
+++ producer_consumer_model/multithread_version.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import threading
+import time
+import logging
+import random
+import queue
+
+logging.basicConfig(level=logging.DEBUG, format="(%(threadName)-9s) %(message)s")
+
+BUF_SIZE = 10
+q = queue.Queue(BUF_SIZE)
+
+
+class ProducerThread(threading.Thread):
+    def __init__(
+        self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None
+    ):
+        super(ProducerThread, self).__init__()
+        self.target = target
+        self.name = name
+
+    def run(self):
+        while True:
+            if not q.full():
+                item = random.randint(1, 10)
+                q.put(item)
+                logging.debug(
+                    "Putting " + str(item) + " : " + str(q.qsize()) + " items in queue"
+                )
+                time.sleep(random.random())
+
+
+class ConsumerThread(threading.Thread):
+    def __init__(
+        self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None
+    ):
+        super(ConsumerThread, self).__init__()
+        self.target = target
+        self.name = name
+
+    def run(self):
+        while True:
+            if not q.empty():
+                item = q.get()
+                logging.debug(
+                    "Getting " + str(item) + " : " + str(q.qsize()) + " items in queue"
+                )
+                time.sleep(random.random())
+
+
+if __name__ == "__main__":
+
+    p = ProducerThread(name="producer")
+    c = ConsumerThread(name="consumer")
+
+    p.start()
+    time.sleep(2)
+    c.start()
+    time.sleep(2)

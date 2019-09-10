7c64d558734687c66fef35c049eb517bc277bca7
simon <1107432420@qq.com>
2019-09-10 09:45:34 +0800

    rewrite lcs sub seq

--- cpp/longest_common_sub_sequence.cpp
+++ cpp/longest_common_sub_sequence.cpp
@@ -2,29 +2,29 @@
 
 using namespace std;
 
-int lcs_sub_seq(char *m, char *n, int l_m, int l_n)
+int lcs_sub_seq(char a[], char b[], int m, int n)
 {
-    int t[l_m + 1][l_n + 1];
-    for (int i = 0; i <= l_m; i++)
+    int t[m + 1][n + 1];
+    for (int i = 0; i <= m; i++)
     {
-        for (int j = 0; j <= l_n; j++)
+        for (int j = 0; j <= n; j++)
         {
             if (i == 0 || j == 0)
                 t[i][j] = 0;
-            else if (m[i - 1] == m[j - 1])
+            else if (a[i - 1] == b[j - 1])
                 t[i][j] = t[i - 1][j - 1] + 1;
             else
                 t[i][j] = max(t[i - 1][j], t[i][j - 1]);
         }
     }
-    return t[l_m][l_n];
+    return t[m][n];
 }
 
 int main(int argc, char const *argv[])
 {
-    char m[] = "liuximin";
-    char n[] = "liuaximin";
+    char a[] = "liuximin";
+    char b[] = "liuaximin";
     cout << "Length of longest common sub sequence is "
-         << lcs_sub_seq(m, n, strlen(m), strlen(n)) << endl;
+         << lcs_sub_seq(a, b, strlen(a), strlen(b)) << endl;
     return 0;
 }

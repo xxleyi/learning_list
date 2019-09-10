44c936beaad7e8fd70534f39f5b1f77bc21bf895
simon <1107432420@qq.com>
2019-09-10 09:28:52 +0800

    add cpp basic part 1: io, char array vs string, char array swap

---
+++ cpp/cpp_basic.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+// basic io: print line and accept line
+void io()
+{
+    using namespace std;
+    cout << "Hello, C++ newbee, please say hello to me: ";
+    string in;
+    getline(cin, in);
+    cout << in << endl;
+}
+
+void char_array_vs_string()
+{
+    using namespace std;
+    char a = 'a';
+    cout << "char a: " << a << endl;
+
+    char b[] = "I am a char array string";
+    cout << "string b: \"" << b << "\" has size: " << strlen(b) << endl;
+
+    string c = "I am a string string";
+    cout << "string c: \"" << c << "\" has size: " << c.size() << endl;
+}
+
+void _swap_str(char a[], int size)
+{
+    for (int i = 0; i < size / 2; i++)
+    {
+        char temp = a[i];
+        a[i] = a[size - i - 1];
+        a[size - i - 1] = temp;
+    }
+}
+
+void swap_str()
+{
+    char odd[] = "odd";
+    std::cout << "before swap: " << odd << std::endl;
+    _swap_str(odd, strlen(odd));
+    std::cout << "after  swap: " << odd << std::endl;
+    char even[] = "even";
+    std::cout << "before swap: " << even << std::endl;
+    _swap_str(even, strlen(even));
+    std::cout << "after  swap: " << even << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    io();
+    char_array_vs_string();
+    swap_str();
+    return 0;
+}

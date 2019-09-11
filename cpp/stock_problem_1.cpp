a65ee193ca5abb8a5cb01caaf3633e77db8cbcb4
simon <1107432420@qq.com>
2019-09-11 21:50:36 +0800

    add stock problem  (1)

---
+++ cpp/stock_problem_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int get_max_profit(int prices[], int size)
+{
+    int max_profit = 0;
+    int min_price = INT_MAX;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (prices[i] < min_price)
+            min_price = prices[i];
+        else if (prices[i] - min_price > max_profit)
+            max_profit = prices[i] - min_price;
+    }
+    
+    return max_profit;
+}
+
+int main(int argc, char const *argv[])
+{   
+    int prices[]{1, 9, 2, 11, 18, 20, 40, 10};
+    cout << "max profit is: " << get_max_profit(prices, sizeof(prices) / sizeof(*prices)) << endl;
+    return 0;
+}

4f28951161348805cae8f7dd30208cb6b8248d41
simon <1107432420@qq.com>
2019-09-14 19:01:01 +0800

    array of class objects

---
+++ cpp/usestok2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "stock1.h"
+
+const int STKS = 4;
+int main(int argc, char const *argv[])
+{
+    using std::cout;
+    using std::ios_base;
+
+    // create an array of initialized objects
+    Stock stocks[STKS] = {
+        Stock("NanoSmart", 12, 20.0),
+        Stock("Boffo Objects", 200, 2.0),
+        Stock("Monolithis Obelisks", 130, 3.25),
+        Stock("Fleep Enterprises", 60, 6.5),
+    };
+
+    cout.precision(2); // #.## format 
+    cout.setf(ios_base::fixed, ios_base::floatfield);// #.## format 
+    cout.setf(ios_base::showpoint); // #.## format
+
+    cout << "Stock holdings:\n";
+    int st;
+    for (st = 0; st < STKS; st++)
+        stocks[st].show();
+
+    Stock top = stocks[0];
+    for (st = 1; st < STKS; st++)
+        top = top.topval(stocks[st]);
+    cout << "\nMost valuable holding:\n";
+    top.show();
+    
+    return 0;
+}

4ea50692259e3155cb46b68c8f1e87adafbefaa8
simon <1107432420@qq.com>
2019-09-14 16:32:19 +0800

    give Stock a constructor: public function function calles Stock

--- cpp/stocks.cpp
+++ cpp/stocks.cpp
@@ -10,12 +10,30 @@ private:
     double total_val;
     void set_tot() { total_val = shares * share_val;  }
 public:
-    void acquire(const char * co, int n, double pr);
+    Stock(const char *co, int n = 0, double pr = 0.0);
+    void acquire(const char *co, int n, double pr);
     void buy(int num, double price);
     void sell(int num, double price);
     void update(double price);
     void show();
 };
+
+Stock::Stock(const char * co, int n, double pr)
+{
+    std::strncpy(company, co, 29);
+    company[29] = '\0';
+
+    if (n < 0)
+    {
+        std::cerr << "Number of shares can't be negative;"
+                  << company << " shares set to 0.\n";
+        shares = 0;
+    }
+    else
+        shares = n;
+    share_val = pr;
+    set_tot();
+}
 
 void Stock::acquire(const char * co, int n, double pr)
 {
@@ -88,7 +106,7 @@ int main(int argc, char const *argv[])
 {
     using std::cout;
     using std::ios_base;
-    Stock stock1;
+    Stock stock1 = Stock("NanoSmart");
     stock1.acquire("NanoSmart", 20, 12.50);
     cout.setf(ios_base::fixed);
     cout.precision(2);
